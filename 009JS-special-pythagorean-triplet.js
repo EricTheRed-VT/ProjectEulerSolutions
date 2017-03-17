25c44ef507105159df80e41670cad96ee510e5da
Eric <ericrainvillevt@gmail.com>
2017-03-16 21:01:42 -0400

    prob 9

--- 009JS-special-pythagorean-triplet.js
+++ 009JS-special-pythagorean-triplet.js
@@ -8,3 +8,22 @@
 ////////////////////////////////////////////////////////
 
 // Approach:
+// we want x = a*b*c, where
+// 0 < a < b < c      and
+// a^2 + b^2 = c^2    and
+// a + b + c = 1000   ;
+
+function specialPythagoreanTriple() {
+  for (let a = 0; a < 333; a++) {
+    for (let b = a + 1; b < 500; b++) {
+      let c = 1000 - a - b;
+      if (a * a + b * b === c * c) {
+        console.log("Found:", a, b, c);
+        return a * b * c;
+      }
+    }
+  }
+  return "Not Found";
+}
+
+console.log(specialPythagoreanTriple())
